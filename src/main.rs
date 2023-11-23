4664248deb862cb2e8b0e5b85bc9022fd784a8b4
mharv <mitch.k.harvey@gmail.com>
2023-11-23 11:49:28 +0000

    edit the concurrency example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    // Ownership and Borrowing
+    let owner_string = String::from("Hello, ownership!");
+    let modified_string = modify_and_return(owner_string);
+    println!("\nOwnership and Borrowing Result: {}", modified_string);
+
+    // Lifetime System
+    let string1: String = String::from("Hello");
+    let string2 = String::from("world!");
+    let result = combine_strings(&string1, &string2, "Rust");
+    println!("\nLifetime System Result: {}", result);
+
+    // Concurrency without Data Races
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            // Introduce a delay to slow down the execution
+            thread::sleep(std::time::Duration::from_secs(1));
+
+            *num += 1;
+
+            let thread_id = thread::current().id();
+
+            // Print the thread ID holding the mutex
+            println!("Thread {:?} acquired the lock", thread_id);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!(
+        "Concurrency without Data Races Result: {}",
+        *counter.lock().unwrap()
+    );
+
+    // Trait System and Pattern Matching
+    let circle = Circle { radius: 5.0 };
+    let rectangle = Rectangle {
+        width: 4.0,
+        height: 6.0,
+    };
+
+    // Using the draw_shape function to draw different shapes
+    println!("\nTrait System and Pattern Matching Result: ");
+    draw_shape(&circle);
+    draw_shape(&rectangle);
+    print_area(&circle);
+    print_area(&rectangle);
+
+    // No Null or Undefined Behavior
+    // let optional_value: Option<i32> = Some(42);
+    let optional_value: Option<i32> = None;
+
+    println!("\nNo Null or Undefined Behavior Result: ");
+    match optional_value {
+        Some(value) => println!("No Null Result: {}", value),
+        None => println!("No value"),
+    }
+
+    // Zero-Cost Abstractions
+    let numbers: Vec<i32> = (1..=5).collect();
+    let squared_numbers: Vec<i32> = square_numbers(numbers);
+    println!("\nZero-Cost Abstractions Result: {:?}", squared_numbers);
+}
+
 // Updated definition of the Drawable trait
 trait Drawable {
     fn draw(&self);
@@ -50,70 +127,6 @@ fn draw_shape(shape: &dyn Drawable) {
 fn print_area(shape: &dyn Drawable) {
     // Pattern matching based on trait methods
     shape.area();
-}
-
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-fn main() {
-    // Ownership and Borrowing
-    let owner_string = String::from("Hello, ownership!");
-    let modified_string = modify_and_return(owner_string);
-    println!("\nOwnership and Borrowing Result: {}", modified_string);
-
-    // Lifetime System
-    let string1: String = String::from("Hello");
-    let string2 = String::from("world!");
-    let result = combine_strings(&string1, &string2, "Rust");
-    println!("\nLifetime System Result: {}", result);
-
-    // Concurrency without Data Races
-    let counter: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
-    let mut handles: Vec<thread::JoinHandle<()>> = vec![];
-    for _ in 0..10 {
-        let counter: Arc<Mutex<i32>> = Arc::clone(&counter);
-        let handle: thread::JoinHandle<()> = thread::spawn(move || {
-            let mut num: std::sync::MutexGuard<'_, i32> = counter.lock().unwrap();
-            *num += 1;
-        });
-        handles.push(handle);
-    }
-    for handle in handles {
-        handle.join().unwrap();
-    }
-    println!(
-        "\nConcurrency without Data Races Result: {}",
-        *counter.lock().unwrap()
-    );
-
-    // Trait System and Pattern Matching
-    let circle = Circle { radius: 5.0 };
-    let rectangle = Rectangle {
-        width: 4.0,
-        height: 6.0,
-    };
-
-    // Using the draw_shape function to draw different shapes
-    println!("\nTrait System and Pattern Matching Result: ");
-    draw_shape(&circle);
-    draw_shape(&rectangle);
-    print_area(&circle);
-    print_area(&rectangle);
-
-    // No Null or Undefined Behavior
-    // let optional_value: Option<i32> = Some(42);
-    let optional_value: Option<i32> = None;
-
-    println!("\nNo Null or Undefined Behavior Result: ");
-    match optional_value {
-        Some(value) => println!("No Null Result: {}", value),
-        None => println!("No value"),
-    }
-
-    // Zero-Cost Abstractions
-    let numbers: Vec<i32> = (1..=5).collect();
-    let squared_numbers: Vec<i32> = square_numbers(numbers);
-    println!("\nZero-Cost Abstractions Result: {:?}", squared_numbers);
 }
 
 // Functions for each concept:
